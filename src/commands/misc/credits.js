ba1548ae53f7ba3f658af89c09ff2b13354bfada
Gavin Wainwright <gav.wainwright@gmail.com>
2018-05-28 16:08:41 -0400

    a

--- src/commands/misc/credits.js
+++ src/commands/misc/credits.js
@@ -12,10 +12,14 @@ module.exports = class CreditsCommand extends Command {
 
   run(msg) {
     const embed = new this.client.embed()
-      .setTitle('Toasty Credits:')
+      .setTitle('Toasty Credits')
       .addField('Developer:', 'i am toast#1213')
+      .addBlankField()
       .addField('Contributors:', 'Dutchy#8775\nOGNovuh#0003\nAceAttorneyMaster111#4489')
+      .addBlankField()
       .addField('Mega Donators:', 'YTAlwaysPlug#9763\nrodcad66#0001\n아빠 [DAD]#5884')
+      .addBlankField()
+      .addField('Graphic Designers:', 'OP#7822')
     msg.embed(embed);
   }
 };
